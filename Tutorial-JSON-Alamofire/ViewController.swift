85228dce608532934e88519033b0c3227caadc52
Programadores-iOS.net <github@programadores-ios.net>
2015-01-19 23:20:44 -0800

    Initial Commit

--- Tutorial-JSON-Alamofire/ViewController.swift
+++ Tutorial-JSON-Alamofire/ViewController.swift
@@ -7,17 +7,51 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var fecha: UILabel!
+    @IBOutlet weak var ipAddress: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        getFecha()
+        getIP()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    
+    
+    func getFecha()
+    {
+        Alamofire.request(.GET, "http://date.jsontest.com").responseJSON() {
+            (_, _, JSON, error) in
+            if error == nil {
+                let info =  JSON as NSDictionary
+                
+                self.fecha.text = info["date"] as? String
+            } else {
+                
+                println(error)
+            }
+        }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    }
+    
+    func getIP()
+    {
+        Alamofire.request(.GET, "http://ip.jsontest.com").responseJSON() {
+            (_, _, JSON, error) in
+            if error == nil {
+                let info =  JSON as NSDictionary
+                self.ipAddress.text = info["ip"] as? String
+            } else {
+                
+                println(error)
+            }
+        }
+        
     }
 
 
